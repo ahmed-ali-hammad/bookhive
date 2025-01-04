bf203c03cc658c9413f9a0e31ae21fef24ae1416
Ahmed Hammad <ahmed.hammad.am@gmail.com>
2025-01-04 17:40:49 +0100

    introduce users module

---
+++ src/users/models.py
@@ -0,0 +1,22 @@
+from sqlmodel import SQLModel, Field, Column
+
+from uuid import UUID
+import sqlalchemy.dialects.postgresql as pg
+
+from datetime import datetime, date
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "user"
+
+    id: UUID = Field(primary_key=True)
+    username: str
+    email: str
+    first_name: str
+    last_name: date
+    is_verified: bool = False
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+
+    def __repr__(self):
+        return f"User {self.first_name} {self.last_name}"
