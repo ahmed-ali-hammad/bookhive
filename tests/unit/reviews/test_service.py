8b42bfae5e6a01361a268ec6db17d11dea63a807
Ahmed Hammad <ahmed.hammad.am@gmail.com>
2025-03-28 23:01:51 +0000

    more tests for review service

--- tests/unit/reviews/test_service.py
+++ tests/unit/reviews/test_service.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.exceptions import UserNotFoundException, BookNotFoundException
 from src.reviews.schemas import ReviewCreateModel
 from src.reviews.service import ReviewService
 
@@ -29,3 +30,37 @@ class TestReviewService:
         assert review.rating == 4
         assert review.user_id == dummy_user.id
         assert review.book_id == dummy_book.id
+
+    @pytest.mark.asyncio
+    async def test_add_new_review_user_not_found(
+        self, mocker, dummy_book, mock_async_db_session
+    ):
+        mocker.patch(
+            "src.users.service.UserService.get_user_by_email",
+            return_value=None,
+        )
+
+        review_data = ReviewCreateModel(text="review text", rating=4)
+
+        with pytest.raises(UserNotFoundException):
+            await review_service.add_new_review(
+                "email@google.de", dummy_book.id, review_data, mock_async_db_session
+            )
+
+    @pytest.mark.asyncio
+    async def test_add_new_review_book_not_found(
+        self, mocker, dummy_user, dummy_book, mock_async_db_session
+    ):
+        mocker.patch(
+            "src.users.service.UserService.get_user_by_email",
+            return_value=dummy_user,
+        )
+
+        mocker.patch("src.books.service.BookService.get_book", return_value=None)
+
+        review_data = ReviewCreateModel(text="review text", rating=4)
+
+        with pytest.raises(BookNotFoundException):
+            await review_service.add_new_review(
+                "email@google.de", dummy_book.id, review_data, mock_async_db_session
+            )
